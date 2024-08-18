3b299a5f78c1220080394118713081a7ff8037ca
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-18 12:20:47 +0300

    remove double quotes

--- comments/wsgi.py
+++ comments/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "comments.settings")
 
 application = get_wsgi_application()
