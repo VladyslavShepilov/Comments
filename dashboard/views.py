059dc621b3181e00671fe9a2ca14f44ff352a8da
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-18 22:29:06 +0300

    add comment model, configure base view and urls

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Comment
 
-# Create your views here.
+
+class CommentsListView(generic.ListView):
+    model = Comment
+    template_name = "dashboard/comments_list.html"
+    context_object_name = "comments"
+    paginate_by = 25
+
+    def get_queryset(self):
+        return Comment.objects.filter(parent__isnull=True)
