1c390087e602c379e2ab120e34374a082990a8ce
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-19 21:38:06 +0300

    add user page

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import User
 
-# Create your views here.
+
+class UserDetailView(generic.DetailView):
+    model = User
+    template_name = "user/user_detail.html"
+    context_object_name = "user"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_replies_count"] = self.object.comments.count()
+        return context
