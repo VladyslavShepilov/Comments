41dc51fb53edacfa6a724e35127dc5d2eca23c74
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-19 16:03:17 +0300

    adjusted view with comment counter query, extend layouts/css, extend script to hide comments

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,22 @@
 function loadReplies(commentId) {
     var replyContainer = document.getElementById("replies-" + commentId);
+    var button = document.getElementById("comment-button-" + commentId);
 
-    if (replyContainer.innerHTML.trim() === "") {
-        fetch(`/dashboard/${commentId}/replies/`)
-            .then(response => response.text())
-            .then(html => {
-                replyContainer.innerHTML = html;
-                replyContainer.style.display = "block";
-            });
+    if (replyContainer.style.display === "none" || replyContainer.style.display === "") {
+        if (replyContainer.innerHTML.trim() === "") {
+            fetch(`/dashboard/${commentId}/replies/`)
+                .then(response => response.text())
+                .then(html => {
+                    replyContainer.innerHTML = html;
+                    replyContainer.style.display = "block";
+                    button.textContent = "Hide replies";
+                });
+        } else {
+            replyContainer.style.display = "block";
+            button.textContent = "Hide replies";
+        }
     } else {
-        replyContainer.style.display = replyContainer.style.display === "none" ? "block" : "none";
+        replyContainer.style.display = "none";
+        button.textContent = `Show replies`;
     }
 }
