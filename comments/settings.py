4c0cc58d54e5801a3cafebf46522e4e53da67ba2
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-19 16:47:37 +0300

    add avatar field to user

--- comments/settings.py
+++ comments/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
 STATIC_ROOT = "staticfiles/"
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+MEDIA_URL = "/files/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- user/models.py
+++ user/models.py
@@ -1,6 +1,23 @@
+import os
+import uuid
+from django.utils.text import slugify
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+def profile_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/user_images/", filename)
+
+
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False, null=False)
+    email = models.EmailField(
+        unique=True, blank=False, null=False
+    )
+    avatar = models.ImageField(
+        upload_to=profile_image_file_path,
+        default="default_profile.png",
+        null=True,
+    )
