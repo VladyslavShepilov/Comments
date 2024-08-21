7044bc60c353204885af0923a4c37337df3f603f
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-21 17:43:49 +0300

    add user registration form

--- user/forms.py
+++ user/forms.py
@@ -1,2 +1,26 @@
-from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import User
+from captcha.fields import CaptchaField
 
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    captcha = CaptchaField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2", "captcha"]
+        widgets = {
+            "password1": forms.PasswordInput(),
+            "password2": forms.PasswordInput(),
+        }
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "avatar"]
+        widgets = {
+            "avatar": forms.ClearableFileInput(attrs={"multiple": False}),
+        }
