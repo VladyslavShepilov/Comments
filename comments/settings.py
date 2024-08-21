647f979e4b49992846a0ce3d3c1806e0dbf5e392
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-21 17:43:49 +0300

    add user registration form

--- comments/settings.py
+++ comments/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "rest_framework",
     "rest_framework_simplejwt",
+    "captcha",
     "user",
     "dashboard",
 ]
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
