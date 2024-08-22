fd4a4d669f2dc0200bc5e18d7289ef3a7335f2bb
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-22 17:09:24 +0300

    minor refactor

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from django.views import generic
 from django.urls import reverse_lazy, reverse
-from django.contrib.auth import login, update_session_auth_hash
-from django.contrib import messages
+from django.contrib.auth import login
 from django.shortcuts import redirect
 
 from .forms import UserRegistrationForm, UserUpdateForm, OptionalPasswordChangeForm
@@ -40,29 +39,36 @@ class UserUpdateView(generic.edit.FormView):
     def get(self, request, *args, **kwargs):
         user_form = UserUpdateForm(instance=request.user)
         password_form = OptionalPasswordChangeForm(user=request.user)
-        return self.render_to_response(self.get_context_data(user_form=user_form, password_form=password_form))
+        return self.render_to_response(
+            self.get_context_data(user_form=user_form, password_form=password_form)
+        )
 
     def post(self, request, *args, **kwargs):
         user_form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
         password_form = OptionalPasswordChangeForm(user=request.user, data=request.POST)
 
-        context = self.get_context_data(user_form=user_form, password_form=password_form)
+        context = self.get_context_data(
+            user_form=user_form, password_form=password_form
+        )
 
         if user_form.is_valid():
             user_form.save()
 
-        password_provided = request.POST.get("new_password1") or request.POST.get("new_password2")
+        password_provided = request.POST.get("new_password1") or request.POST.get(
+            "new_password2"
+        )
         if password_provided:
             if password_form.is_valid():
                 password_form.save()
             else:
                 context["password_errors"] = password_form.errors
 
-        if not user_form.is_valid() or (password_provided and not password_form.is_valid()):
+        if not user_form.is_valid() or (
+            password_provided and not password_form.is_valid()
+        ):
             return redirect(self.get_success_url())
 
         return redirect(self.get_success_url())
 
     def get_success_url(self):
         return reverse("user-detail", kwargs={"pk": self.request.user.pk})
-
