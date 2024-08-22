62d1ccb072f4466c0dda05dd056a4fe1064d4f0b
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-22 14:46:28 +0300

    add forms and views for user managing

--- comments/urls.py
+++ comments/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from debug_toolbar import urls as debug_toolbar_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("captcha/", include("captcha.urls")),
     path("dashboard/", include("dashboard.urls")),
     path("", include("user.urls")),
+    path("__debug__/", include(debug_toolbar_urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
