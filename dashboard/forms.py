3663c2d360999dbb2f7120582065ab724b168cbb
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-25 10:21:16 +0300

    add text field validation for xhtml

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+import bleach
 from captcha.fields import CaptchaField
+from django.core.exceptions import ValidationError
+
 from .models import Comment
+
+
+ALLOWED_TAGS = {"a", "code", "i", "strong"}
+ALLOWED_ATTRIBUTES = {
+    "a": ["href", "title"]
+}
 
 
 class CommentForm(forms.ModelForm):
@@ -9,6 +18,22 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["text", "parent", "image", "captcha"]
+
+    def clean_text(self):
+        text = self.cleaned_data.get("text", "")
+        cleaned_text = bleach.clean(
+            text,
+            tags=ALLOWED_TAGS,
+            attributes=ALLOWED_ATTRIBUTES,
+            strip=True,
+        )
+
+        try:
+            bleach.linkify(cleaned_text)
+        except Exception as e:
+            raise ValidationError("Unable to convert : {} ".format(e))
+
+        return cleaned_text
 
     def save(self, commit=True, user=None):
         comment = super().save(commit=False)
