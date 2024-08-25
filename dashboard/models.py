bf0437c0d4ae77657fb52921e7e4a74b01d8fc4f
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-25 09:52:59 +0300

    add picture field

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,24 @@
+import os
+import uuid
+from django.utils.text import slugify
+
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
+def text_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/comment_images/", filename)
+
+
 class Comment(models.Model):
     text = models.TextField()
+    image = models.ImageField(
+        upload_to=text_image_file_path,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         get_user_model(), related_name="comments", on_delete=models.CASCADE
