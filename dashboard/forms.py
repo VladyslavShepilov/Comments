7a0aa8f9c1d2ff4f52a64bc06a1db13e1c62ffb2
Vladyslav Shepilov <shepsterya@gmail.com>
2024-08-25 20:14:17 +0300

    extend form to handle both files and images

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,9 +2,15 @@ from django import forms
 import bleach
 from captcha.fields import CaptchaField
 from django.core.exceptions import ValidationError
-
 from .models import Comment
-
+from .validators import (
+    validate_image_extension,
+    validate_image_size,
+    validate_image_file,
+    validate_text_file_size,
+    validate_text_file_extension,
+)
+from PIL import Image
 
 ALLOWED_TAGS = {"a", "code", "i", "strong"}
 ALLOWED_ATTRIBUTES = {
@@ -17,7 +23,7 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ["text", "parent", "captcha", "image"]
+        fields = ["text", "parent", "captcha", "file"]
 
     def clean_text(self):
         text = self.cleaned_data.get("text", "")
@@ -27,16 +33,26 @@ class CommentForm(forms.ModelForm):
             attributes=ALLOWED_ATTRIBUTES,
             strip=True,
         )
-
-        try:
-            bleach.linkify(cleaned_text)
-        except Exception as e:
-            raise ValidationError("Unable to convert : {} ".format(e))
-
         return cleaned_text
 
     def save(self, commit=True, user=None):
         comment = super().save(commit=False)
+        uploaded_file = self.cleaned_data.get("file")
+        if uploaded_file:
+            try:
+                img = Image.open(uploaded_file)
+                img.verify()
+                validate_image_extension(uploaded_file)
+                validate_image_size(uploaded_file)
+                validate_image_file(uploaded_file)
+                comment.image = uploaded_file
+                comment.file = None
+            except (IOError, SyntaxError, ValidationError):
+                validate_text_file_extension(uploaded_file)
+                validate_text_file_size(uploaded_file)
+                comment.file = uploaded_file
+                comment.image = None
+
         if user:
             comment.user = user
         if commit:
